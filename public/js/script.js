49e19761676c3a2a5b8d55f63cf3b55925ef03e9
Srinivas Rohit Nethi <srinivasrohitnethi@Srinivass-MacBook-Pro.local>
2018-02-20 21:11:39 -0500

    add layout upto projects

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function(){
+
+    var options = {
+        strings: ["Designer.", "Developer."],
+        typeSpeed: 100,
+        typeDelay: 1600,
+        showCursor: false,
+        loop: true,
+        loopCount: 0,
+        backSpeed: 100,
+        backDelay: 2000
+      }
+
+      $( ".heading_primary--sub--typed" ).typed(options);
+});
